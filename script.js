32cd31cb1b26004d73666a7f774b8e323d379f9e
Ptrck <pat@gmail.com>
2020-09-28 14:49:48 +0800

    complete w/ spraypaint

---
+++ script.js
@@ -0,0 +1,25 @@
+const canvas = document.getElementById('canvas')
+
+const ctx = canvas.getContext('2d')
+
+let size = 30
+let x = 50
+let y = 50
+
+
+canvas.addEventListener('mousedown', (e) => {
+    const x = e.offsetX
+    const y = e.offsetY
+
+    drawCircle(x, y)
+})
+
+function drawCircle(x, y) {
+    ctx.beginPath()
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fill()
+}
+
+
+
+
